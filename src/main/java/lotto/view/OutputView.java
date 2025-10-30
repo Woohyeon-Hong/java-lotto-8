b9ab6fdc3754715baec5805d4108db891dced071
woohyeon <woohyeon6885@gmail.com>
2025-10-30 22:47:48 +0900

    fix: 로또 구매 수량 출력 형식 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -12,7 +12,7 @@ public class OutputView {
     }
 
     public void printLottoCount(int lottoCount) {
-        System.out.println(lottoCount + "개를 구매했습니다.");
+        System.out.println("\n" + lottoCount + "개를 구매했습니다.");
     }
 
     public void printLottoNumbers(Lotto lotto) {
