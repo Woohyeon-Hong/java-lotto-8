de262fe1648458c77ef5e61f58f21f3b0461f58e
woohyeon <woohyeon6885@gmail.com>
2025-11-02 18:28:53 +0900

    refactor(InputView): 예외 메시지를 상수로 분리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,11 +9,27 @@ import java.util.List;
 /**
  * TODO
  * 1. Validator, Parser 분리하기
- * 2. 예외 메시지 상수로 분리하기
  */
 public class InputView {
 
     private static final String LOTTO_NUMBER_DELIMITER = ",";
+
+    //예외 메시지
+    public static final String DUPLICATE_BONUS_NUMBER_ERROR_MESSAGE =
+            "[ERROR] 입력하신 보너스 번호가 이미 당첨번호에 포함돼 있습니다.";
+    public static final String NOT_A_NUMBER_ERROR_MESSAGE =
+            "[ERROR] 입력하신 값이 숫자가 아닙니다.";
+    public static final String INVALID_LOTTO_COUNT_ERROR_MESSAGE =
+            "[ERROR] 당첨 번호는 6개여야 합니다.";
+    public static final String OUT_OF_RANGE_NUMBER_ERROR_MESSAGE =
+            "[ERROR] 입력하신 숫자는 1에서 45 사이의 값이어야 합니다.";
+    public static final String DUPLICATE_LOTTO_NUMBER_ERROR_MESSAGE =
+            "[ERROR] 당첨 번호는 서로 달라야 합니다.";
+    public static final String NOT_NATURAL_NUMBER_ERROR_MESSAGE =
+            "[ERROR] 입력하신 값이 자연수가 아닙니다.";
+    public static final String NOT_MULTIPLE_OF_THOUSAND_ERROR_MESSAGE =
+            "[ERROR] 입력하신 금액이 1000원 단위가 아닙니다.";
+
 
 
     public InputView() {
@@ -53,7 +69,7 @@ public class InputView {
 
     public void validateBonusNumberNotInWinningNumbers(List<Integer> winningNumbers, int number) {
         if (winningNumbers.contains(number)) {
-            throw new IllegalArgumentException("[ERROR] 입력하신 보너스 번호가 이미 당첨번호에 포함돼 있습니다.");
+            throw new IllegalArgumentException(DUPLICATE_BONUS_NUMBER_ERROR_MESSAGE);
         }
     }
 
@@ -63,7 +79,7 @@ public class InputView {
         try {
             purchaseAmount = Integer.parseInt(numberInput);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 입력하신 값이 숫자가 아닙니다.");
+            throw new IllegalArgumentException( NOT_A_NUMBER_ERROR_MESSAGE);
         }
 
         return purchaseAmount;
@@ -76,31 +92,31 @@ public class InputView {
 
     public void validateLottoNumberCount(List<Integer> winningNumbers) {
         if (winningNumbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(INVALID_LOTTO_COUNT_ERROR_MESSAGE);
         }
     }
 
     public void validateLottoNumberRange(int number) {
         if (number > 45 || number < 1) {
-            throw new IllegalArgumentException("[ERROR] 입력하신 숫자는 1에서 45 사이의 값이어야 합니다.");
+            throw new IllegalArgumentException(OUT_OF_RANGE_NUMBER_ERROR_MESSAGE);
         }
     }
 
     private void validateUniqueLottoNumbers(List<Integer> winningNumbers) {
         if (new HashSet<Integer>(winningNumbers).size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 서로 달라야 합니다.");
+            throw new IllegalArgumentException(DUPLICATE_LOTTO_NUMBER_ERROR_MESSAGE);
         }
     }
 
     private void validateNaturalNumber(int number) {
         if (number <= 0) {
-            throw new IllegalArgumentException("[ERROR] 입력하신 값이 자연수가 아닙니다.");
+            throw new IllegalArgumentException(NOT_NATURAL_NUMBER_ERROR_MESSAGE);
         }
     }
 
     private void validateMultipleOfThousand(int purchaseAmount) {
         if (purchaseAmount % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 입력하신 금액이 1000원 단위가 아닙니다.");
+            throw new IllegalArgumentException(NOT_MULTIPLE_OF_THOUSAND_ERROR_MESSAGE);
         }
     }
 }
